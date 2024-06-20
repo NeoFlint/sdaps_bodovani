8e5c71a31e62ca9b860db9e41f920d917e0d9876
NeoFlint <31320379+NeoFlint@users.noreply.github.com>
2024-06-20 23:05:22 +0200

    just cosmetic changes at main script..

--- zpracuj_sdaps.py
+++ zpracuj_sdaps.py
@@ -7,12 +7,12 @@
 import io, os
 from collections import OrderedDict
 from csv import reader
-import pyexcel as pe 						# pip install pyexcel
+import pyexcel as pe 					# pip install pyexcel
 from pyexcel_ods import save_data			# pip install pyexcel_ods
-import pandas								# pip install pandas
+import pandas						# pip install pandas
 
-template_file = 'spravne_odpovedi.ods'		# klic spravnych odpovedi - pripraven jako rozsifrovany!
-input_file = 'data_sdaps.csv'				# SDAPS data z recognice - prvni na radce musi byt cislo respondenta!
+template_file = 'spravne_odpovedi.ods'			# klic spravnych odpovedi - pripraven jako rozsifrovany!
+input_file = 'data_sdaps.csv'				# SDAPS data z recognice - prejmenovat soubor nebo upravit jeho jmeno zde
 output_file = 'vysledky.ods'				# zde budou celkove vysledky prijimacich zkousek - upravit a ZASIFROVAT!
 
 # -------------------------------
@@ -37,15 +37,15 @@ def load_inputs():
 	correct_answers = sheet.to_dict()
 	#print("Spravne odpovedi:")
 	#for key, value in correct_answers.items():
-		#print(key, value[0], value[1])						# value[0] = bodova hodnota, value[1] = spravna odpoved(pismeno)
+		#print(key, value[0], value[1])					# value[0] = bodova hodnota, value[1] = spravna odpoved(pismeno)
 	
 	# nacteni odpovedi uchazecu z SDAPS
 	csv_data = []
 	with open(input_file, 'r') as csv_file:					# as csv_file je dulezite - diky tomu to nacte
 		csv_reader = reader(csv_file, delimiter=",")
-		for line in csv_reader:								# workarround - jak lepe?
+		for line in csv_reader:						# workarround - jak lepe?
 			csv_data.append(line)
-	#for row in csv_data:									# prehled nacteneho obsahu CSV
+	#for row in csv_data:							# prehled nacteneho obsahu CSV
 		#print(row)
 
 	# dale pristup k datum za predpokladu stejneho indexu - muzeme si to dovolit, protoze CSV ma urcite shodny pocet prvku v radcich
@@ -58,7 +58,7 @@ def set_question_header(csv_header):
 		chunks = item.split('_')
 		if 'rev' in item:
 			continue
-		elif len(chunks) > 3:								# jednickami rozebrane a,b,c,d u odpovedi do headeru nepotrebujeme
+		elif len(chunks) > 3:						# jednickami rozebrane a,b,c,d u odpovedi do headeru nepotrebujeme
 			continue	
 		else:
 			question_header.append(item)
@@ -72,10 +72,10 @@ def data_processing(csv_data=None, correct_answers=None):
 		print("ERROR: Neplatne vstupy ke zpracovani!")
 		return
 		
-	marks = OrderedDict()				# zaklad pro budouci vystupy
-	csv_header = csv_data[0]			# hlavicka v CSV
-	question_header = set_question_header(csv_header)		# pouze zahlavi k otazkam - index bude roven globalnimu cislu otazky
-	col_number = len(csv_header)		# kolik je sloupcu v CSV
+	marks = OrderedDict()					# zaklad pro budouci vystupy
+	csv_header = csv_data[0]				# hlavicka v CSV
+	question_header = set_question_header(csv_header)	# pouze zahlavi k otazkam - index bude roven globalnimu cislu otazky
+	col_number = len(csv_header)				# kolik je sloupcu v CSV
 	
 	if len(question_header)-1 == len(correct_answers):
 		print("Kontrola poctu spravnych odpovedi a poctu otazek ve vstupnich souborech: OK")
@@ -83,7 +83,7 @@ def data_processing(csv_data=None, correct_answers=None):
 		print("POZOR!!! Pocet spravnych odpovedi a celkovy pocet otazek ve vstupnich souborech NESOUHLASI!!!")
 		return
 
-	for line in csv_data[1:]:			# zahlavi se vynechava, neobsahuje data
+	for line in csv_data[1:]:				# zahlavi se vynechava, neobsahuje data
 
 		# rozbor metadat odpovedi
 		# id = line[0] 					# pokud CSV obsahuje skutecna questionnaire_id
@@ -93,7 +93,7 @@ def data_processing(csv_data=None, correct_answers=None):
 		recognized = line[4]
 		verified = line[6]
 		
-		if id in marks.keys():			# pokud nebylo upraveno ID uchazece, SDAPS generuje do csv hodnotu 1 - nutno resit a v SDAPS GUI doplnit!
+		if id in marks.keys():				# pokud nebylo upraveno ID uchazece, SDAPS generuje do csv hodnotu 1 - nutno resit a v SDAPS GUI doplnit!
 			print(f"ID {id} jiz existuje. Preskakuji... Zkontrolujte ID ucastniku ve vstupnich souborech!")
 			continue
 
@@ -103,37 +103,34 @@ def data_processing(csv_data=None, correct_answers=None):
 		sum = 0
 
 		# ted samotne odpovedi (q_num = cislo otazky)
-		#for q_num in range(1,len(question_header)):
-		for i in range(12,col_number,1): 								# prvni cast jsou identifikacni informace --> preskakujeme
-			if 'rev' in csv_header[i]: 									# review sloupce data neobsahuje --> preskakujeme
+		for i in range(12,col_number,1): 							# prvni cast jsou identifikacni informace --> preskakujeme
+			if 'rev' in csv_header[i]: 							# review sloupce data neobsahuje --> preskakujeme
 				continue
 			else:
-				# zjisteni podrobnosti k otazce rozparsovanim jejiho headeru
-				chunks = csv_header[i].split('_')
-				if len(chunks) > 3:										# jednickami rozebrane a,b,c,d u odpovedi preskakujeme - informaci uz vime
+				chunks = csv_header[i].split('_')					# zjisteni podrobnosti k otazce rozparsovanim jejiho headeru
+				if len(chunks) > 3:							# jednickami rozebrane a,b,c,d u odpovedi preskakujeme - informaci uz vime
 					continue	
 				test = int(chunks[0])
 				section = int(chunks[1])
 				question = int(chunks[2])
-				question_number = question_header.index(csv_header[i])				# zjisteni globalniho cisla otazky
+				question_number = question_header.index(csv_header[i])			# zjisteni globalniho cisla otazky
 
 				# nacteni spravne odpovedi a zaznam bodu za otazku
 				value, correct_choice = correct_answers[str(question_number)]		# klic je ve slovniku bohuzel v podobe stringu
-				answer_num = int(line[i]) if line[i].isdigit() else 0				# pokud je v odpovedi neciselna hodnota --> ztracena otazka, 0b
-				answer = choices[answer_num]										# drivejsi SDAPS daval jen ciselnou hodnotu odpovedi, ted uz tu mame i textove vystupy
+				answer_num = int(line[i]) if line[i].isdigit() else 0			# drivejsi SDAPS daval jen ciselnou hodnotu odpovedi, ted uz tu mame i textove vystupy
+				answer = choices[answer_num]						
 				# print(f"id: {id}, question_number: {question_number}, answer_num: {answer_num}, answer: {answer}, correct_choice: {correct_choice}")
 				
 				if(correct_choice == answer):
 					values[question_number] = value
 					sum += value
 				else:
-					values[question_number] = 0
+					values[question_number] = 0					# neciselna hodnota odpovedi je proste 0b bez dalsiho zkoumani
 
-		values['bodovy_zisk']=sum
+		values['bodovy_zisk']=sum								# pridani obodovaneho dotazniku do datoveho slovniku
 		marks[id] = values
 
 	return marks
-
 
 # Ulozi zpracovane vysledky do tabulkoveho souboru.
 def make_output(marks, jmena=None):
